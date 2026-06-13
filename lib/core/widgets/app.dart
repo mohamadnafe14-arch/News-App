eb2bd6e61ac706e2d450d57b56e800d82adfd558
mohamed <mnafe44@gmail.com>
2026-06-13 13:01:11 +0300

    adding home view

---
+++ lib/core/widgets/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/features/home/view/home_view.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeView(),
+    );
+  }
+}
